cf61582b98ee21deb36ab560a90f9931aed18395
valery <valery.fremaux@gmail.com>
2016-10-12 17:31:29 +0200

    Automated sync from SVN

--- userequip_form.php
+++ userequip_form.php
@@ -153,7 +153,9 @@ class UserEquipmentForm extends moodleform {
                 foreach ($modules as $mod) {
                     $mname = $mod->name;
                     $module = $DB->get_record('modules', array('name' => $mname));
-                    if (empty($module) || !$module->visible) continue;
+                    if (empty($module) || !$module->visible) {
+                        continue;
+                    }
                     $mform->addElement('checkbox', 'mod_'.$mname, get_string('pluginname', $mname));
                     $allplugins[] = 'mod_'.$mname;
                 }
